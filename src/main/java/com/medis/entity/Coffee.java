69229f31cca6f3d2a8d975dadcf1295d778904b0
Thanos <2325098522@qq.com>
2020-08-27 20:14:41 +0800

    Initial commit

---
+++ src/main/java/com/medis/entity/Coffee.java
@@ -0,0 +1,32 @@
+package com.medis.entity;
+
+public class Coffee {
+    private String id;
+    private String name;
+
+    /************************************* Constructor *************************************/
+    public Coffee() {
+    }
+
+    public Coffee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    /************************************ Getter Setter ************************************/
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
