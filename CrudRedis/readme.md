7f3d72068f7375ab83be7539b83f1aff685dc7eb
Thanos <2325098522@qq.com>
2020-12-17 13:17:59 +0800

    添加redis测试代码

--- CrudRedis/readme.md
+++ CrudRedis/readme.md
@@ -1,6 +1,14 @@
 # Redis curd操作
-#### Redis存放数据类型
+### Redis存放数据类型
 > Redis支持5种数据类型,但是redis存放的是二进制数据,也就是字节数组(byte[])
 这些字节数据是没有类型的,使用合理的格式解码后,可以变成一个字符串,整数,或对象,此时才有数据类型
 所以只要能转化成字节数组(byte[])的,都可以存放到redis里面,不管是字符串,数字,对象,图片,声音,视频,文件等
 因此Redis的String并不是指字符串,redis里的key和value都是byte数组
+
+### Develop Standard
+1.key名设计要以业务名(或数据库名)为前缀(防止key冲突)，用冒号分隔,业务名:表名:id,`ugc:video:1`
+2.不要包含空格、换行、单双引号以及其他转义字符
+3.value设计
++ string类型控制在10KB以内，hash、list、set、zset元素个数不要超过5000
++ 非字符串的bigkey，不要使用del删除，使用hscan、sscan、zscan方式渐进式删除
++ 防止bigkey过期时间自动删除造成阻塞
--- CrudRedis/src/test/java/com/sprmedis/crud/TestRedis.java
+++ CrudRedis/src/test/java/com/sprmedis/crud/TestRedis.java
@@ -1,5 +1,6 @@
 package com.sprmedis.crud;
 
+import com.alibaba.fastjson.JSON;
 import com.sprmedis.crud.component.StringRedisUtil;
 import com.sprmedis.crud.entity.UserInfo;
 import org.junit.Test;
@@ -11,6 +12,10 @@ import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -19,13 +24,13 @@ public class TestRedis {
     @Resource
     StringRedisUtil redisUtil;
     @Resource
-    private RedisTemplate redisTemplate;
+    private RedisTemplate<String, Object> redisTemplate;
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
     @Test
-    public void testSave(){
-        UserInfo sysUser = new UserInfo("1233","李3");
+    public void testSave() {
+        UserInfo sysUser = new UserInfo("1233", "李3");
         ValueOperations vOps = redisTemplate.opsForValue();
         vOps.set("zxu2", sysUser, 6000L, TimeUnit.SECONDS);
         UserInfo sysUserDB = (UserInfo) vOps.get("zxu2");
@@ -33,18 +38,77 @@ public class TestRedis {
     }
 
     @Test
-    public void testStrValue(){
+    public void testStrValue() {
         ValueOperations<String, String> ops4Value = stringRedisTemplate.opsForValue();
         ops4Value.set("key", "value");
         ops4Value.set("key", "value", 1L);
         ops4Value.get("key");
 
     }
+
     @Test
     public void testSelect() {
         String chat = redisUtil.getRedis("chat");
         System.out.println(chat);
         redisUtil.setRedis("sdw", "sbssb");
         System.out.println(redisUtil.getRedis("sdw"));
+    }
+
+    /**
+     * 性能测试:保存用户信息
+     * + 有三种方式
+     * 1.利用 HMSET 和 HGET 哈希表的方式
+     * 2.利用 set key value(用户信息序列化后的信息)
+     * 3.利用 set key value(用户信息转成的JSON)
+     * + 经过更多的数据测试发现 利用RedisTemplate的方式操作redis, 使用存JSON的方式速度最快，存哈希表的方式最慢
+     */
+    @Test
+    public void testSaveUserInfoJSON() {
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 20000; i++) {
+            Map<String, Object> userInfo = new HashMap<>();
+            String userId = "u003" + i;
+            userInfo.put("name", "zxu");
+            userInfo.put("age", 11);
+            userInfo.put("sex", 1);
+            
+            List<Map> aaa = new ArrayList<>();
+            for (int j = 0; j < 10; j++) {
+                Map<String, Object> inner = new HashMap<>();
+                inner.put("AAA", "AAA");
+                inner.put("AAA1", "AAA");
+                inner.put("AAA2", "AAA");
+                inner.put("AAA3", "AAA");
+                inner.put("AAA4", "AAA");
+                aaa.add(inner);
+            }
+            List<Map> bbb = new ArrayList<>();
+            for (int j = 0; j < 10; j++) {
+                Map<String, Object> inner = new HashMap<>();
+                inner.put("BBB", 12345);
+                inner.put("BBB1", 12345);
+                inner.put("BBB2", 12345);
+                inner.put("BBB3", 12345);
+                inner.put("BBB4", 12345);
+                inner.put("BBB5", 12345);
+                aaa.add(inner);
+            }
+            userInfo.put("aaa", aaa);
+            userInfo.put("bbb", bbb);
+             /*// 利用 HMSET 和 HGET 哈希表的方式 20759s
+            redisTemplate.opsForHash().putAll(userId, userInfo);
+            Map o = redisTemplate.opsForHash().entries(userId);
+            */
+            /* // 利用 set key value(用户信息序列化后的信息) 20731s
+            redisTemplate.opsForValue().set(userId, userInfo);
+            Map o1 = (Map) redisTemplate.opsForValue().get(userId);
+            */
+             // 利用 set key value(用户信息转成的JSON) 17372s
+            String toJSON = JSON.toJSONString(userInfo);
+            redisTemplate.opsForValue().set(userId, toJSON);
+            String o = (String)redisTemplate.opsForValue().get(userId);
+            Map map = JSON.parseObject(o, Map.class);
+        }
+        System.out.println(System.currentTimeMillis() - start);
     }
 }
